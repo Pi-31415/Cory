6e926a27e7fd3f9e06691675f78bec0394b12973
Pi <paingthetko.pi@gmail.com>
2020-05-19 21:36:40 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,18 @@
+const colorarray = ['#F44336','#E91E63','#9C27B0','#2196F3','#00897B','#43A047','#FB8C00','#795548'];
+const colornames = ['Red','Pink','Purple','Blue','Teal','Green','Orange','Brown']
+
+//Colour picker
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 function setup() {
   createCanvas(displayWidth, displayHeight);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 20, 20);
+	strokeWeight(0);
+	fill(colorarray[randomIntFromInterval(0,colorarray.length-1)]);
+	ellipse(mouseX, mouseY, 20, 20);
+	
 }
